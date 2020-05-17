73d9b4cf7c25c4a37813b49713ddcb301c96f474
frezirovschik <frezirovschik@DESKTOP-FPFUT5P>
2020-05-18 01:29:54 +0300

    Add project files.

---
+++ alg_lab7/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace alg_lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dirPath = new System.Windows.Forms.TextBox();
+            this.btn_goto = new System.Windows.Forms.Button();
+            this.itemslistBox = new System.Windows.Forms.ListBox();
+            this.btn_goBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dirPath
+            // 
+            this.dirPath.Location = new System.Drawing.Point(0, 0);
+            this.dirPath.Name = "dirPath";
+            this.dirPath.Size = new System.Drawing.Size(360, 22);
+            this.dirPath.TabIndex = 0;
+            // 
+            // btn_goto
+            // 
+            this.btn_goto.Location = new System.Drawing.Point(407, 0);
+            this.btn_goto.Name = "btn_goto";
+            this.btn_goto.Size = new System.Drawing.Size(75, 23);
+            this.btn_goto.TabIndex = 1;
+            this.btn_goto.Text = "go to";
+            this.btn_goto.UseVisualStyleBackColor = true;
+            this.btn_goto.Click += new System.EventHandler(this.btn_goto_Click);
+            // 
+            // itemslistBox
+            // 
+            this.itemslistBox.FormattingEnabled = true;
+            this.itemslistBox.ItemHeight = 16;
+            this.itemslistBox.Location = new System.Drawing.Point(0, 51);
+            this.itemslistBox.Name = "itemslistBox";
+            this.itemslistBox.Size = new System.Drawing.Size(482, 372);
+            this.itemslistBox.TabIndex = 2;
+            this.itemslistBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.images_listBox_MouseDoubleClick);
+            // 
+            // btn_goBack
+            // 
+            this.btn_goBack.Location = new System.Drawing.Point(407, 29);
+            this.btn_goBack.Name = "btn_goBack";
+            this.btn_goBack.Size = new System.Drawing.Size(75, 23);
+            this.btn_goBack.TabIndex = 3;
+            this.btn_goBack.Text = "go back";
+            this.btn_goBack.UseVisualStyleBackColor = true;
+            this.btn_goBack.Click += new System.EventHandler(this.btn_goBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 450);
+            this.Controls.Add(this.btn_goBack);
+            this.Controls.Add(this.itemslistBox);
+            this.Controls.Add(this.btn_goto);
+            this.Controls.Add(this.dirPath);
+            this.Name = "Form1";
+            this.Text = "image view";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dirPath;
+        private System.Windows.Forms.Button btn_goto;
+        private System.Windows.Forms.ListBox itemslistBox;
+        private System.Windows.Forms.Button btn_goBack;
+    }
+}
+
---
+++ alg_lab7/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace alg_lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_goto_Click(object sender, EventArgs e)
+        {
+            itemslistBox.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(dirPath.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo d in dirs) {
+                itemslistBox.Items.Add(d);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo f in files)
+            {
+                itemslistBox.Items.Add(f);
+            }
+        }
+
+        private void images_listBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(dirPath.Text, itemslistBox.SelectedItem.ToString())) == "")
+            {
+                dirPath.Text = Path.Combine(dirPath.Text, itemslistBox.SelectedItem.ToString());
+                itemslistBox.Items.Clear();
+                DirectoryInfo dir = new DirectoryInfo(dirPath.Text);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                foreach (DirectoryInfo d in dirs)
+                {
+                    itemslistBox.Items.Add(d);
+                }
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo f in files)
+                {
+                    itemslistBox.Items.Add(f);
+                }
+            }
+            else {
+                Process.Start(Path.Combine(dirPath.Text, itemslistBox.SelectedItem.ToString()));
+            }
+            /*dirPath.Text = Path.Combine(dirPath.Text, images_listBox.SelectedItem.ToString());
+            images_listBox.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(dirPath.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo d in dirs)
+            {
+                images_listBox.Items.Add(d);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo f in files)
+            {
+                images_listBox.Items.Add(f);
+            }*/
+        }
+
+        private void btn_goBack_Click(object sender, EventArgs e)
+        {
+            if (dirPath.Text[dirPath.Text.Length - 1] == '\\')
+            {
+                dirPath.Text = dirPath.Text.Remove(dirPath.Text.Length - 1, 1);
+                while (dirPath.Text[dirPath.Text.Length - 1] == '\\')
+                {
+                    dirPath.Text = dirPath.Text.Remove(dirPath.Text.Length - 1, 1);
+                }
+            }
+            else if (dirPath.Text[dirPath.Text.Length - 1] != '\\')
+            {
+                while (dirPath.Text[dirPath.Text.Length - 1] != '\\')
+                {
+                    dirPath.Text = dirPath.Text.Remove(dirPath.Text.Length - 1, 1);
+                }
+            }
+            else {
+                while (dirPath.Text[dirPath.Text.Length - 1] != '\\')
+                {
+                    dirPath.Text = dirPath.Text.Remove(dirPath.Text.Length - 1, 1);
+                }
+            }
+        }
+    }
+}
